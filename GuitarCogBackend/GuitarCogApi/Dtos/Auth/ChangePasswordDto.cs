9e8151339a0f476b2b9c476ddc9bbce003ad0710
Ravil <graf.rav@gmail.com>
2022-12-04 12:33:10 +0300

    [gc-70] change password

---
+++ GuitarCogBackend/GuitarCogApi/Dtos/Auth/ChangePasswordDto.cs
@@ -0,0 +1,13 @@
+namespace GuitarCogApi.Dtos.Auth;
+
+public class ChangePasswordDto
+{
+    public ChangePasswordDto(string oldPassword, string newPassword)
+    {
+        OldPassword = oldPassword;
+        NewPassword = newPassword;
+    }
+
+    public string OldPassword { get; set; }
+    public string NewPassword { get; set; }
+}
