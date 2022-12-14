7b73723b45880e0eb2f03879c7acfc13a02e41cc
Ravil <graf.rav@gmail.com>
2022-12-14 21:52:24 +0300

    dto fixes

--- GuitarCogBackend/GuitarCogApi/Dtos/Tab/TabListDto.cs
+++ GuitarCogBackend/GuitarCogApi/Dtos/Tab/TabListDto.cs
@@ -2,20 +2,26 @@ namespace GuitarCogApi.Dtos.Tab;
 
 public class TabListDto
 {
-    public TabListDto()
+    public TabListDto(string fileUrl, string authorName)
     {
+        FileUrl = fileUrl;
+        AuthorName = authorName;
     }
 
-    public TabListDto(long id, string name, string authorId, DateTimeOffset loadDateTime)
+    public TabListDto(long id, string name, string fileUrl, string authorId, string authorName, DateTimeOffset loadDateTime)
     {
         Id = id;
         Name = name;
         AuthorId = authorId;
         LoadDateTime = loadDateTime;
+        AuthorName = authorName;
+        FileUrl = fileUrl;
     }
 
     public long Id { get; set; }
     public string Name { get; set; }
+    public string FileUrl { get; set; }
     public string AuthorId { get; set; }
+    public string AuthorName { get; set; }
     public DateTimeOffset LoadDateTime { get; set; }
 }
