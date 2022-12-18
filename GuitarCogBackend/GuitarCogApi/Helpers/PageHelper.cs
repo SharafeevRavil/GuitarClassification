10984ed4f93561c333352819a328613c78b96124
Ravil <graf.rav@gmail.com>
2022-12-18 17:48:24 +0300

    [gc-115] limit pagination

--- GuitarCogBackend/GuitarCogApi/Helpers/PageHelper.cs
+++ GuitarCogBackend/GuitarCogApi/Helpers/PageHelper.cs
@@ -7,8 +7,20 @@ public static class PageHelper
 {
     public static async Task<PagedResponse<T>> PagedResponse<T>(this IQueryable<T> data, int page, int pageSize)
     {
-        var respData = await data.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+        if (page <= 0) page = 1;
+        if (pageSize == 0)
+            throw new ArgumentException(nameof(pageSize));
+
         var count = await data.CountAsync();
+        if (count == 0)
+            return new PagedResponse<T>(new List<T>(), 1, 1, 0);
+
+        var minElementsCountToCurPage = (page - 1) * pageSize + 1;
+        if (count < minElementsCountToCurPage)
+            //не хватает элементов - возвращаю последнюю страницу
+            page = (count - 1) / pageSize + 1;
+
+        var respData = await data.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
         
         var pageCount = (int)Math.Ceiling((double)count / pageSize);
         return new PagedResponse<T>(respData, page, pageCount, count);
